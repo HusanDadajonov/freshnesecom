e44f55f2277971a3c31045d78a38d886103cd44b
am1rcek <xusandadajonov4385@gmail.com>
2021-10-21 21:01:55 -0700

    awd

--- js/main.js
+++ js/main.js
@@ -4,11 +4,9 @@ let elLineOne = document.querySelector(".line1");
 let elLineThree = document.querySelector(".line3");
 let elNav = document.querySelector(".nav");
 let elHeaderTop = document.querySelector(".header-top");
-let elBody = document.querySelector("body");
 
 
 elHeaderMenu.addEventListener("click",function(){
-    elBody.classList.toggle("hidden-scrol");
     elLinethird.classList.toggle("remove-line");
     elLineOne.classList.toggle("rotate-line1");
     elLineThree.classList.toggle("rotate-line3");
