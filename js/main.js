439731f2525b7b05d0abb4430665c70a66e113f1
am1rcek <xusandadajonov4385@gmail.com>
2021-10-26 01:29:01 -0700

    awd

--- js/main.js
+++ js/main.js
@@ -4,6 +4,10 @@ let elLineOne = document.querySelector(".line1");
 let elLineThree = document.querySelector(".line3");
 let elNav = document.querySelector(".nav");
 let elHeaderTop = document.querySelector(".header-top");
+let elListbtn = document.querySelector(".fruits__btn--list");
+let elGridbtn = document.querySelector(".fruits__btn--grid");
+let elSmallList = document.querySelector(".categories__list--small");
+let elBiglList = document.querySelector(".categories__list--big");
 
 
 elHeaderMenu.addEventListener("click",function(){
@@ -14,3 +18,12 @@ elHeaderMenu.addEventListener("click",function(){
     elHeaderTop.classList.toggle("open-header");
 });
 
+elListbtn.addEventListener("click",function(){
+    elBiglList.classList.add("remove__list--big");
+    elSmallList.classList.add("show-list-small");
+});
+
+elGridbtn.addEventListener("click",function(){
+    elBiglList.classList.remove("remove__list--big");
+    elSmallList.classList.remove("show-list-small");
+});
